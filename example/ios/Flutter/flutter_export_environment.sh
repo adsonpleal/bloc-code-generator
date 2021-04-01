3250cfb845f48bedfbd5c6c52c01adf26366021c
your_email@example.com <your_email@example.com>
2021-04-02 00:02:56 +0800

    支持flutter 2.0

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/shengqiang/flutter_beta"
+export "FLUTTER_ROOT=/Users/shengqiang/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/shengqiang/Documents/Codes/bloc-code-generator/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/shengqiang/flutter_beta/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
--- lib/src/bloc_generator.dart
+++ lib/src/bloc_generator.dart
@@ -6,31 +6,32 @@ import 'package:source_gen/source_gen.dart';
 import '../annotations.dart';
 
 class _EventMethod {
-  final String event;
-  final String name;
-  final MethodElement method;
-
-  _EventMethod({this.name, this.event, this.method});
+    final String event;
+    final String name;
+    final MethodElement method;
+    
+    _EventMethod({required this.name, required this.event, required this.method});
 }
 
 class BlocGenerator extends GeneratorForAnnotation<GenerateBloc> {
-  @override
-  String generateForAnnotatedElement(
-    Element element,
-    ConstantReader annotation,
-    BuildStep buildStep,
-  ) {
-    final eventMethods = _eventMethods(element);
-    final stateType = annotation.read('stateType').typeValue;
-    return '''
+    @override
+    String generateForAnnotatedElement(
+        Element element,
+        ConstantReader annotation,
+        BuildStep buildStep,
+        ) {
+        
+        final eventMethods = _eventMethods(element as ClassElement);
+        final stateType = annotation.read('stateType').typeValue;
+        return '''
     ${_eventEnum(eventMethods)}
     $_eventClass
     ${_blocClass(eventMethods, stateType)}
     ''';
-  }
-
-  String _blocClass(List<_EventMethod> eventMethods, DartType stateType) {
-    return '''
+    }
+    
+    String _blocClass(List<_EventMethod> eventMethods, DartType stateType) {
+        return '''
     abstract class _\$Bloc extends Bloc<_\$Event,${stateType.name}> {\n
       ${_construct(stateType)}
       ${_mapToStateMethod(eventMethods, stateType)}
@@ -38,33 +39,33 @@ class BlocGenerator extends GeneratorForAnnotation<GenerateBloc> {
       ${eventMethods.map((em) => _abstractMethod(em, stateType)).join('\n')}
     }
     ''';
-  }
-  
-  String _construct(DartType stateType) {
-    return '''
+    }
+    
+    String _construct(DartType stateType) {
+        return '''
     _\$Bloc(${stateType.name} initialState) : super(initialState);
     ''';
-  }
-
-  String _abstractMethod(_EventMethod eventMethod, DartType stateType) {
-    final params = eventMethod.method.parameters
-        .map((p) => "${p.type.displayName} ${p.name}")
-        .join(',');
-    return '''
+    }
+    
+    String _abstractMethod(_EventMethod eventMethod, DartType stateType) {
+        final params = eventMethod.method.parameters
+            .map((p) => "${p.type.displayName} ${p.name}")
+            .join(',');
+        return '''
     Stream<${stateType.name}> ${eventMethod.method.name}($params);
     ''';
-  }
-
-  String _dispatchMethod(_EventMethod eventMethod) {
-    final methodParams = eventMethod.method.parameters;
-    final params =
-        methodParams.map((p) => "${p.type.displayName} ${p.name}").join(',');
-    String payload = '';
-    if (methodParams.isNotEmpty) {
-      final args = methodParams.map((p) => p.name).join(',');
-      payload = 'payload: [$args],';
     }
-    return '''
+    
+    String _dispatchMethod(_EventMethod eventMethod) {
+        final methodParams = eventMethod.method.parameters;
+        final params =
+        methodParams.map((p) => "${p.type.displayName} ${p.name}").join(',');
+        String payload = '';
+        if (methodParams.isNotEmpty) {
+            final args = methodParams.map((p) => p.name).join(',');
+            payload = 'payload: [$args],';
+        }
+        return '''
     void dispatch${eventMethod.name}Event($params) {
       add(_\$Event(
         type: _\$EventType.${eventMethod.event},
@@ -72,15 +73,15 @@ class BlocGenerator extends GeneratorForAnnotation<GenerateBloc> {
       ));
     }
     ''';
-  }
-
-  String _eventEnum(List<_EventMethod> eventMethods) => '''
+    }
+    
+    String _eventEnum(List<_EventMethod> eventMethods) => '''
     enum _\$EventType {
       ${eventMethods.map((em) => em.event).join(',')}
     }  
   ''';
-
-  String get _eventClass => '''
+    
+    String get _eventClass => '''
     class _\$Event {
       final _\$EventType type;
       final List<dynamic> payload;
@@ -88,37 +89,40 @@ class BlocGenerator extends GeneratorForAnnotation<GenerateBloc> {
       _\$Event({this.type, this.payload});
     }  
   ''';
-
-  String _mapToStateMethod(
-      List<_EventMethod> eventMethods, DartType stateType) {
-    final cases = eventMethods.map((em) {
-      final params = em.method.parameters
-          .asMap()
-          .keys
-          .map((index) => 'event.payload[$index]')
-          .join(',');
-      return '''
+    
+    String _mapToStateMethod(
+        List<_EventMethod> eventMethods, DartType stateType) {
+        final cases = eventMethods.map((em) {
+            final params = em.method.parameters
+                .asMap()
+                .keys
+                .map((index) => 'event.payload[$index]')
+                .join(',');
+            return '''
         case _\$EventType.${em.event}:
           yield* ${em.method.name}($params);
           break;''';
-    }).join('\n');
-    return '''
+        }).join('\n');
+        return '''
       Stream<${stateType.name}> mapEventToState(_\$Event event) async* {
         switch(event.type) {
           $cases
         }
       }
     ''';
-  }
-
-  List<_EventMethod> _eventMethods(ClassElement element) {
-    final mapMethodRegExp = RegExp(r"_map(.*)ToState");
-    final mapMethods = element.methods
-      ..removeWhere((m) => !mapMethodRegExp.hasMatch(m.name));
-    return mapMethods.map((m) {
-      String name = mapMethodRegExp.firstMatch(m.name).group(1);
-      final event = name[0].toLowerCase() + name.substring(1);
-      return _EventMethod(event: event, method: m, name: name);
-    }).toList();
-  }
+    }
+    
+    List<_EventMethod> _eventMethods(ClassElement element) {
+        final mapMethodRegExp = RegExp(r"_map(.*)ToState");
+        final mapMethods = element.methods
+            ..removeWhere((m) => !mapMethodRegExp.hasMatch(m.name));
+        return mapMethods.map((m) {
+            String? name = mapMethodRegExp.firstMatch(m.name)?.group(1);
+            if (name != null) {
+                final event = name[0].toLowerCase() + name.substring(1);
+                return _EventMethod(event: event, method: m, name: name);
+            }
+            return _EventMethod(event: "", method: m, name: "");
+        }).toList();
+    }
 }
